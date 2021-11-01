cef8d48f5be1ee6d16545b4a68a43df470e0c5b6
maruthi@12345 <maruthiprasadkandela@gmail.com>
2021-11-01 07:43:58 +0530

    updated terraform

---
+++ var.tf
@@ -0,0 +1,41 @@
+
+variable "resource_group_name" {
+  description = ""
+  default     = ""
+}
+
+variable "location" {
+  description = ""
+  default     = ""
+}
+
+variable "default_tags" {
+  description = ""
+  #type        = ""
+  default     = {}
+}
+
+variable "address_space" {
+  description = ""
+  default     = ""
+}
+
+variable "subnet" {
+  description = ""
+  default     = ""
+}
+
+variable "application" {
+  description = ""
+  default     = ""
+}
+
+variable "environment" {
+  description = ""
+  default     = ""
+}
+
+variable "capacity" {
+  description = ""
+  default     = ""
+}
